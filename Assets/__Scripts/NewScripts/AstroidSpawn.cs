a259d408851906557e647997230c0d12c5a48873
alexp230 <apruitt230@gmail.com>
2025-02-20 15:46:06 -0600

    Ship and Bullet now collide with the asteroids

--- Assets/__Scripts/NewScripts/AstroidSpawn.cs
+++ Assets/__Scripts/NewScripts/AstroidSpawn.cs
@@ -11,8 +11,6 @@ public class AstroidSpawn : MonoBehaviour
     void Start()
     {
         ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, this.transform.position.z));
-
-        print(ScreenBounds);
     }
 
     void FixedUpdate()
@@ -29,8 +27,7 @@ public class AstroidSpawn : MonoBehaviour
     {
         int x = Random.Range((int)(-ScreenBounds.x), (int)(ScreenBounds.x+1));
         int y = Random.Range((int)(-ScreenBounds.y), (int)(ScreenBounds.y+1));
-        // print("Position = " + new Vector3(x, y, ScreenBounds.z));
-        return new Vector3(x, y, ScreenBounds.z);
+        return new Vector3(x, y, 0);
     }
 
 
