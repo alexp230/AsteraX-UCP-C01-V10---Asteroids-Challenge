26f1ff3a8e497ed6fc8c13bba4ae796000eee311
alexp230 <apruitt230@gmail.com>
2025-03-06 10:54:31 -0600

    Game now ends after all lives lost and reloads

--- Assets/__Scripts/NewScripts/UITracker.cs
+++ Assets/__Scripts/NewScripts/UITracker.cs
@@ -1,12 +1,13 @@
-using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UITracker : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI PlayerScoreText;
     [SerializeField] private TextMeshProUGUI PlayerLivesText;
-    
+    [SerializeField] private RawImage EndGameScreen;    
 
     public void SetPlayerScoreText(int score)
     {
@@ -15,7 +16,16 @@ public class UITracker : MonoBehaviour
     public void SetPlayerLivesText(int lives)
     {
         if (lives < 0)
+        {
+            EndGameScreen.gameObject.SetActive(true);
+            Invoke(nameof(RestartGame), 3f);
             return;
+        }
         PlayerLivesText.text = $"Lives Left: {lives}";
+    }
+    private void RestartGame()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
     }
 }
